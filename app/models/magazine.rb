1a8973385545912619bd38d9c375f8f4b813ebfc
Isaac <izaroot327@gmail.com>
2021-05-25 11:15:55 -0400

    Completed Moc

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,25 @@
 class Magazine < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        emails = []
+        self.readers.each do |reader|
+            emails << reader.email 
+        end
+        emails.join(";")
+    end
+
+    def self.most_popular
+        sub_count = 0
+        best_magzine = {}
+        self.all.each do |magazine|
+            if magazine.subscriptions.count >= sub_count
+                sub_count = magazine.subscriptions.count
+                best_magzine = magazine
+            end
+        end
+        puts sub_count
+        best_magzine
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,26 @@
 class Reader < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions
+
+    def subscribe (magazine, price)
+        Subscription.create(reader_id: self.id, magazine_id: magazine.id, price: price)    
+    end
+
+    def total_subscription_price
+        total = []
+        self.subscriptions.each do |subscription|
+            total << subscription.price   
+        end
+        total.sum
+    end
+
+    def cancel_subscription(magazine)
+        to_cancel = []
+        self.subscriptions.each do |subscription|
+            if subscription.magazine_id == magazine.id
+                to_cancel << subscription.id
+            end
+        end
+        Subscription.destroy(to_cancel)
+    end
 end
